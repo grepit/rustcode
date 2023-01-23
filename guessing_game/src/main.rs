f7d4822400a2069fedafb129ab1ce7b71c208266
mountain <aghamoa@yahoo.com>
2023-01-22 22:16:15 -0800

    finished chapter 2

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -7,30 +7,36 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    // --snip--
-
-    println!("The secret number is: {secret_number}");
+   // println!("The secret number is: {secret_number}");
 
     loop {
         println!("Please input your guess.");
 
-        // --snip--
-
-
         let mut guess = String::new();
+
+        // --snip--
 
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         println!("You guessed: {guess}");
+
+        // --snip--
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => println!("You win!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
         }
     }
 }
+
