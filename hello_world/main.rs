a5ac9adde2880c018a90d5b9cc1cb9a92878a4b4
mountain <aghamoa@yahoo.com>
2023-01-29 22:01:55 -0800

    adding the data types

--- hello_world/main.rs
+++ hello_world/main.rs
@@ -1,8 +1,23 @@
+
 fn main() {
     println!("Hello, world!");
 let x = 5;
 let y = 10;
 
 println!("x = {x} and y + 2 = {}", y + 2);
+    let spaces = "   ";
+println!("see {}",spaces);
 
+let spaces = spaces.len();
+
+
+println!("see {}",spaces);
+
+let guess: u32 = "42".parse().expect("Not a number!");
+println!("guess {}",guess);
+   let fees = 25_000;
+   let  salary:f64 = 35_000.00;
+   println!("fees is {} and salary is {}",fees,salary);
+
+let guess1: u32 = 42.parse().expect("Not a number!");
 }
