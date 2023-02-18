76f00286c6bcbd708f4c846757d4bcfc8975ba5d
mountain <aghamoa@yahoo.com>
2023-02-17 21:30:37 -0800

    chapter 4-1

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,9 +1,38 @@
+//find type of object
+// fn print_type_of<T>(_: &T) {
+//     println!("{}", std::any::type_name::<T>())
+// }
+
 fn main() {
-    println!("Hello, world!");
+ let a: String = "hello rust".into();
+ //let a: &str = "hello rust";
 
-    another_function();
+   // let location:&str = "US";
+   println!("-->: {} ",a);
+  
+  chapter4_1()
+
 }
 
-fn another_function() {
-    println!("Another function.");
-}
+fn chapter4_1() {
+    let s = String::from("hello");
+
+    takes_ownership4_1(&s);             
+                              
+
+     println!("{}",s) ; 
+
+    let x = 5;                     
+    makes_copy4_1(x);                 
+} 
+
+ fn takes_ownership4_1(some_string: &str){
+ 
+//fn takes_ownership(some_string: String) { // some_string comes into scope
+    println!("{}", some_string);
+} // Here, some_string goes out of scope and `drop` is called. The backing
+  // memory is freed.
+
+fn makes_copy4_1(some_integer: i32) { // some_integer comes into scope
+    println!("{}", some_integer);
+} // Here, some_integer goes out of scope. Nothing special happens.
