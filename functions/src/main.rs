46f46c9b8210d93b424c12c63fd2c4b1595fb8eb
mountain <aghamoa@yahoo.com>
2023-03-19 23:16:33 -0700

    chapter 6

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -4,70 +4,70 @@
 // }
 
 fn main() {
- let a: String = "hello rust".into();
- //let a: &str = "hello rust";
+    let some_number = Some(5);
+    let some_char = Some('e');
 
-   // let location:&str = "US";
-   println!("-->: {} ",a);
-  
-
- let mut s = String::from("hello");
- let r3 = &mut s; // no problem
-  chapter4_2(r3);
-   let mut sb = String::from("hello world");
-   sb.clear();
-  first_word(&sb);
-
-
-
-    let sd = String::from("hello world");
-
-    let hello = &sd[0..5];
-    let world = &sd[6..11];
-    println!("a {}",hello);
-    println!("b {}",world);
-
+    let absent_number: Option<i32> = None;
 }
+
+//fn main() {
+//     let a: String = "hello rust".into();
+//     //let a: &str = "hello rust";
+
+//     // let location:&str = "US";
+//     println!("-->: {} ", a);
+
+//     let mut s = String::from("hello");
+//     let r3 = &mut s; // no problem
+//     chapter4_2(r3);
+//     let mut sb = String::from("hello world");
+//     sb.clear();
+//     first_word(&sb);
+
+//     let sd = String::from("hello world");
+
+//     let hello = &sd[0..5];
+//     let world = &sd[6..11];
+//     println!("a {}", hello);
+//     println!("b {}", world);
+// }
 
 fn chapter4_1() {
     let s = String::from("hello");
 
-    takes_ownership4_1(&s);             
-                              
+    takes_ownership4_1(&s);
 
-     println!("{}",s) ; 
+    println!("{}", s);
 
-    let x = 5;                     
-    makes_copy4_1(x);                 
-} 
+    let x = 5;
+    makes_copy4_1(x);
+}
 
- fn takes_ownership4_1(some_string: &str){
- 
-//fn takes_ownership(some_string: String) { // some_string comes into scope
+fn takes_ownership4_1(some_string: &str) {
+    //fn takes_ownership(some_string: String) { // some_string comes into scope
     println!("{}", some_string);
 } // Here, some_string goes out of scope and `drop` is called. The backing
   // memory is freed.
 
-fn makes_copy4_1(some_integer: i32) { // some_integer comes into scope
+fn makes_copy4_1(some_integer: i32) {
+    // some_integer comes into scope
     println!("{}", some_integer);
 } // Here, some_integer goes out of scope. Nothing special happens.
 
-
-fn chapter4_2(some_string:  & mut str){
-    println!("see {}",some_string);
+fn chapter4_2(some_string: &mut str) {
+    println!("see {}", some_string);
     let mut abc = some_string.to_string();
     abc.push_str("hhh");
- println!("hi {}",abc)   
-    
+    println!("hi {}", abc)
 }
 
 fn first_word(s: &String) -> usize {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
-        println!("item {}",i);
+        println!("item {}", i);
         if item == b' ' {
-          println!("found space {}",i);
+            println!("found space {}", i);
             return i;
         }
     }
--- struct_model/src/main.rs
+++ struct_model/src/main.rs
@@ -1,9 +1,24 @@
-//chapter 5.2
-
 #[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+    //Notice this acts as constructor ....and returns a instance of the object
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
 }
 
 fn main() {
@@ -11,10 +26,17 @@ fn main() {
         width: 30,
         height: 50,
     };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
 
-    println!("rect1 is {:?}", rect1);
-    //Pretty print
-    println!("rect1 is {:#?}", rect1);
-
-    
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+    let sq = Rectangle::square(3);
+    println!("here {},",sq.width)
 }
