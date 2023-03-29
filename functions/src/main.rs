56b817572e031a051a35bbb647976b86965fedc2
mountain <aghamoa@yahoo.com>
2023-03-28 18:57:25 -0700

    example of valdiating user input

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,76 +1,37 @@
-//find type of object
-// fn print_type_of<T>(_: &T) {
-//     println!("{}", std::any::type_name::<T>())
-// }
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+
 
 fn main() {
-    let some_number = Some(5);
-    let some_char = Some('e');
+    println!("Guess the number!");
 
-    let absent_number: Option<i32> = None;
-}
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-//fn main() {
-//     let a: String = "hello rust".into();
-//     //let a: &str = "hello rust";
+    loop {
+        println!("Please input your guess.");
 
-//     // let location:&str = "US";
-//     println!("-->: {} ", a);
+        let mut guess = String::new();
 
-//     let mut s = String::from("hello");
-//     let r3 = &mut s; // no problem
-//     chapter4_2(r3);
-//     let mut sb = String::from("hello world");
-//     sb.clear();
-//     first_word(&sb);
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-//     let sd = String::from("hello world");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-//     let hello = &sd[0..5];
-//     let world = &sd[6..11];
-//     println!("a {}", hello);
-//     println!("b {}", world);
-// }
+        println!("You guessed: {guess}");
 
-fn chapter4_1() {
-    let s = String::from("hello");
-
-    takes_ownership4_1(&s);
-
-    println!("{}", s);
-
-    let x = 5;
-    makes_copy4_1(x);
-}
-
-fn takes_ownership4_1(some_string: &str) {
-    //fn takes_ownership(some_string: String) { // some_string comes into scope
-    println!("{}", some_string);
-} // Here, some_string goes out of scope and `drop` is called. The backing
-  // memory is freed.
-
-fn makes_copy4_1(some_integer: i32) {
-    // some_integer comes into scope
-    println!("{}", some_integer);
-} // Here, some_integer goes out of scope. Nothing special happens.
-
-fn chapter4_2(some_string: &mut str) {
-    println!("see {}", some_string);
-    let mut abc = some_string.to_string();
-    abc.push_str("hhh");
-    println!("hi {}", abc)
-}
-
-fn first_word(s: &String) -> usize {
-    let bytes = s.as_bytes();
-
-    for (i, &item) in bytes.iter().enumerate() {
-        println!("item {}", i);
-        if item == b' ' {
-            println!("found space {}", i);
-            return i;
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
         }
     }
-
-    s.len()
 }
