68cb84182efa9a71cb2f9bb0414443658124ca9a
mountain <aghamoa@yahoo.com>
2023-04-09 22:59:51 -0700

    strings again !

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -10,5 +10,24 @@ fn main() {
     v.push(6);
 
     println!("The first element is: {first}");
+
+
+//imutable print
+    let v = vec![100, 32, 57];
+    for i in &v {
+        println!("{i}");
+    }
+
+//mutable print 
+   let mut v = vec![100, 32, 57];
+    for i in &mut v {
+        *i += 50;
+    }
+println!("{:?}", v);
+
+   let mut s1 = String::from("foo");
+    let s2 = "bar";
+    s1.push_str(s2);
+    println!("s1 is {s1}");
 }
 
