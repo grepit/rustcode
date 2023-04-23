064ff3894c0d9617cbab02d15df69836bd888a65
mountain <aghamoa@yahoo.com>
2023-04-22 22:57:37 -0700

    error_exceptions

--- error_handle/src/main.rs
+++ error_handle/src/main.rs
@@ -1,29 +1,29 @@
-
+#![allow(unused)]
 fn main() {
-    
-fn divide(a: i32, b: i32) -> Result<i32, String> {
-    if b == 0 {
-        return Err(String::from("Cannot divide by zero!"));
-    }
-    Ok(a / b)
+pub struct Guess {
+    value: i32,
 }
 
 
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
 
-    let dividend = 10;
-    let divisor = 2;
-
-    // Using Option
-    let result_option = dividend.checked_div(divisor);
-    match result_option {
-        Some(result) => println!("Result using Option: {}", result),
-        None => println!("Cannot divide by zero using Option!"),
+        Guess { value }
     }
 
-    // Using Result
-    let result_result = divide(dividend, divisor);
-    match result_result {
-        Ok(result) => println!("Result using Result: {}", result),
-        Err(err) => println!("Error using Result: {}", err),
+    pub fn value(&self) -> i32 {
+        self.value
     }
+}
+
+  let guess = Guess::new(50000);
+    println!("Guess value is {}.", guess.value());
+// let here : i32=100;
+// let guess= Guess(
+//     here,
+//     );
+
 }
