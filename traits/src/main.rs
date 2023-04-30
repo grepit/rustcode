d660bda37627ece236c1cf0aa81612ee554826d7
mountain <aghamoa@yahoo.com>
2023-04-29 17:44:05 -0700

    first trait

---
+++ traits/src/main.rs
@@ -0,0 +1,15 @@
+use aggregator::{Summary, Tweet};
+
+
+fn main() {
+    let tweet = Tweet {
+        username: String::from("horse_ebooks world"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
+}
