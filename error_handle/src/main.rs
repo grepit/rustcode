33b16dad4989d58b7f44ed7018d020c58a8e9a0e
mountain <aghamoa@yahoo.com>
2023-09-06 22:01:53 -0700

    adding error and exception tempelates

--- error_handle/src/main.rs
+++ error_handle/src/main.rs
@@ -1,5 +1,60 @@
-#![allow(unused)]
+enum MyError {
+    Error {
+        message: String,
+    },
+}
+
+//Example check for integer and throw an exception
+fn divide(x: i32, y: i32) -> Result<i32, MyError> {
+    if y == 0 {
+        return Err(MyError::Error { message: "generic erro by zero".to_string() });
+    }
+
+    Ok(x / y)
+}
+
+//Example check for string 
+fn string_wild(string: &str)  -> Result<usize, String> {
+    if string.is_empty() {
+        return Err("The string is empty.".to_string());
+    }
+    else if string.contains("hi"){
+         return Err("The string is not suppose to have hi.".to_string());
+    }
+
+    return Ok(string.len());
+}
+
+
 fn main() {
+    let result = divide(10, 0);
+
+    match result {
+        Ok(value) => println!("The result is {}", value),
+        Err(MyError::Error { message }) => {
+            println!("An error occurred: {}", message);
+        }
+    }
+
+
+
+let mystring = "Hello hi, world!";
+let length = string_wild(&mystring);
+
+match length {
+    Ok(value) => println!("The length of the string is {}.", value),
+    Err(error) => println!("An error occurred: {}", error),
+}
+
+
+}
+
+
+
+
+
+
+/*
 pub struct Guess {
     value: i32,
 }
@@ -21,9 +76,5 @@ impl Guess {
 
   let guess = Guess::new(50000);
     println!("Guess value is {}.", guess.value());
-// let here : i32=100;
-// let guess= Guess(
-//     here,
-//     );
+*/
 
-}
