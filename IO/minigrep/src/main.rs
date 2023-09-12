a882ca8c437aa09c80c5f44e333f01dbb61a26d7
mountain <aghamoa@yahoo.com>
2023-09-11 20:56:32 -0700

    chapter 12.4

--- IO/minigrep/src/main.rs
+++ IO/minigrep/src/main.rs
@@ -1,17 +1,15 @@
-
-use std::env;
-use std::fs;
-use std::process;
-use std::error::Error;
+#[cfg(debug)]
+//use std::process;
+//use std::error::Error;
 
 /*
-How to run 
+How to run
 cargo run -- searchstring example-filename.txt
 cargo run -- the poem.txt
 
 */
 
-    
+/*
 fn main() {
     // --snip--
 
@@ -54,5 +52,70 @@ impl Config {
         let file_path = args[2].clone();
 
         Ok(Config { query, file_path })
+    }
+}
+*/
+
+//refactored version
+// https://doc.rust-lang.org/book/ch12-03-improving-error-handling-and-modularity.html#creating-a-constructor-for-config
+/*
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args);
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    let contents = fs::read_to_string(config.file_path)
+        .expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    // --snip--
+    fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("not enough arguments");
+        }
+        // --snip--
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Config { query, file_path }
+    }
+}
+*/
+use std::env;
+use std::env;
+use std::error::Error;
+use std::fs;
+use std::process;
+
+use minigrep::Config;
+
+fn main() {
+    // --snip--
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    if let Err(e) = minigrep::run(config) {
+        // --snip--
+        println!("Application error: {e}");
+        process::exit(1);
     }
 }
